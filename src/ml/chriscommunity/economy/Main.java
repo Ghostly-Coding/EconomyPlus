da763fee80376ba61a5a767354372ffd956bc7d5
GhostlyCoding <pokeball.181818@gmail.com>
2020-10-25 12:23:51 -0500

    Changed stuff

--- src/ml/chriscommunity/economy/Main.java
+++ src/ml/chriscommunity/economy/Main.java
@@ -1,6 +1,5 @@
 package ml.chriscommunity.economy;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -9,13 +8,7 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		Economy econ = new Economy();
-		if(econ.initEcon() == 0) {
-			Bukkit.getLogger().info("Connected to Database Successfully!");
-		} else {
-			Bukkit.getLogger().info("Failed to connect to Database.");
-			this.getServer().getPluginManager().disablePlugin(this);
-		}
+		
 	}
 	
 	@Override
